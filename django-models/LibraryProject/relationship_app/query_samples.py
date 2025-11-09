d18d6a815636609346844ba1345c6ffce5e05e23
benoansah2025 <memphisperla@gmail.com>
2025-11-09 14:36:17 +0000

    modified librarian to suit the checker

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -38,7 +38,7 @@ def get_books_in_library(library_name):
     
 # 3️⃣ Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
+    library = Librarian.objects.get(library=library_name)
    
 
 def get_all_books():
