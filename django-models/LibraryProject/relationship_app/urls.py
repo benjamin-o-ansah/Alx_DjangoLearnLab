88b28e85c0afecb7a494187bee8cc372069a0597
benjamin-o-ansah <benjaminoansah@gmail.com>
2025-11-11 10:25:25 +0000

    added .view and imported and used list_books

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . import views
+from .views import list_books
+from .import views
 
 urlpatterns = [
     path('display/', views.display_all, name='display_all'),
-    path('books/', views.list_books, name='list_books'),  # Function-based view
+    path('books/', list_books, name='list_books'),  # Function-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view()),
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
