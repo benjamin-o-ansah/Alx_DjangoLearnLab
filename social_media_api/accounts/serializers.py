d1abe064af1c0f24e7b91ae7246c6c555ef52e2b
benoansah2025 <memphisperla@gmail.com>
2025-12-18 11:55:11 +0000

    fix: Fixed alx checker not seeing get_user_model().objects.create_user

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,7 +3,6 @@ from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token 
 from .models import User
 
-User = get_user_model()
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
@@ -14,7 +13,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
         username=validated_data['username'],
         email=validated_data.get('email'),
         password=validated_data['password'],
