d8b71d09d560f9f480c62175ede4f294d0d668df
benoansah2025 <memphisperla@gmail.com>
2025-12-22 12:22:23 +0000

    added tagWidget to forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from .models import Profile,Post,Comment,Tag
+from taggit.forms import TagWidget
 
 User = get_user_model()
 
@@ -40,6 +41,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content","tags"]
+        widgets = {
+            "tags": TagWidget(),
+        }
     
     def save(self, commit=True):
         post = super().save(commit=False)
