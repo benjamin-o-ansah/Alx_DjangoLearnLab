3a2e4ae857f01d4dcf89903bf7cae70e73346051
benoansah2025 <memphisperla@gmail.com>
2025-12-22 12:20:38 +0000

    downloaded and install django taggit to handle tags

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from taggit.managers import TaggableManager
 # Create your models here.
 
 class Tag(models.Model):
@@ -17,7 +17,7 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE  
     )
-    tags = models.ManyToManyField(Tag, blank=True, related_name="posts")
+    tags = TaggableManager(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
